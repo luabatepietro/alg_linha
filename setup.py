2e55dba3bc54427a2691c610b89e21788f9d92fb
luabatepietro <luabatepietroapple@icloud.com>
2024-08-26 14:46:56 -0300

    teste

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     },
     entry_points={
         "console_scripts": [
-            "alg_linha=alg_linha.main:main",  # Apontando para o arquivo main.py corretamente
+            "alg_linha=main:main",  # Apontando para o arquivo main.py corretamente
         ],
     },
     author="Lucas Abatepietro",  # Seu nome
