a22d17183e8febe3efaa4869ee85d20ca2a0e92e
luabatepietro <luabatepietroapple@icloud.com>
2024-08-26 14:53:59 -0300

    fix

--- build/lib/alg_linha/star.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import os
-class Star(pygame.sprite.Sprite):
-    def __init__(self, x: float, y: float, base_image_path) -> None:
-        super().__init__()
-        self.image = pygame.image.load(os.path.join(base_image_path, 'planet_6.png')).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (100, 100))  # Redimensiona se necessário
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
